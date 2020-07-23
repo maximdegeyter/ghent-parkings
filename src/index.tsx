a4d408f65220ceb24bd116343a8226548facbdc3
maximdegeyter <maximdegeyter@outlook.com>
2020-07-23 19:12:23 +0200

    🔥 delete: remove useless import

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import store from "./store";
 
 ReactDOM.render(
   <BrowserRouter>
